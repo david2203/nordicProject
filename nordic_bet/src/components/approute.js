b863494d949b1da70ee17a91d4662e88f4bf749c
Shawn Örnerstedt <Shawn.oernerstedt@emea.merkleinc.com>
2021-10-15 11:18:09 +0200

    no message

--- nordic_bet/src/components/approute.js
+++ nordic_bet/src/components/approute.js
@@ -1,9 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import Register from "./Register";
-=======
 import Games from "./Games"
->>>>>>> e6a9d20af4e2ca8e78565c543d5d5e125e2a1efe
 
 import {
     BrowserRouter as Router,
@@ -17,11 +14,8 @@ function AppRoute() {
         <>
          
         <Router>
-<<<<<<< HEAD
           <Route path="/Register" exact component={Register}/>
-=======
           <Route path="/games" exact component={Games} />
->>>>>>> e6a9d20af4e2ca8e78565c543d5d5e125e2a1efe
         </Router>
 
     
