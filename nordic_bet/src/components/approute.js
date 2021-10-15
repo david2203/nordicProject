16bde26502872a4c9680fb73d16f8a0b856e017f
Shawn Örnerstedt <Shawn.oernerstedt@emea.merkleinc.com>
2021-10-15 10:14:06 +0200

    react

---
+++ nordic_bet/src/components/approute.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+    BrowserRouter as Router,
+    Route
+  } from "react-router-dom";
+
+
+function AppRoute() {
+      return (
+        <>
+         
+        <Router>
+        </Router>
+
+    
+
+        </>
+      )
+  }
+
+export default AppRoute;
