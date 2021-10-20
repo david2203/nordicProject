ddd118a7cd9c996f0d317ccc383053476d23359a
David Saupe <david.saupe@emea.merkleinc.com>
2021-10-20 16:43:55 +0200

    no message

--- nordic_bet/src/components/Game.js
+++ nordic_bet/src/components/Game.js
@@ -3,9 +3,6 @@ import axios from "axios";
 import Modal from 'react-modal';
 import server from "./config"
 import Flags from 'country-flag-icons/react/3x2'
-import {hasFlag} from 'country-flag-icons'
-import { countries } from 'country-flag-icons'
-import getUnicodeFlagIcon from 'country-flag-icons/unicode'
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -32,8 +29,166 @@ function Game({event_id,eid_xml,eventname,grp,odds_1,odds_x,odds_2,status}) {
     const [layBetIsOpen,setLayBetIsOpen] = useState(false);
     const user_id = localStorage.getItem("user_id")
     const token = localStorage.getItem("jwt")
+    const [homeFlag, setHomeFlag] = useState("AQ")
+    const [awayFlag, setAwayFlag] = useState("AQ")
+
+    
+
+    useEffect(()=> {
+        const fetchGameId = async()=>{
+            const response = await instance.get(`Euro_events?eid_xml=${event_id}`)
+            setGameId(response.data[0].id)
+        }
+        fetchGameId()
+        if (home_team === "Poland"){
+            setHomeFlag("PL")
+        } 
+        else if (home_team === "Albania") {
+           setHomeFlag("AL");
+        }
+        else if (home_team === "England") {
+            setHomeFlag("GB");
+         }
+         else if (home_team === "Germany") {
+            setHomeFlag("DE");
+         }
+         else if (home_team === "France") {
+            setHomeFlag("FR");
+         }
+         else if (home_team === "Romania") {
+            setHomeFlag("RO");
+         }
+         else if (home_team === "Switzerland") {
+            setHomeFlag("CH");
+         }
+         else if (home_team === "Wales") {
+            setHomeFlag("WS");
+         }
+         else if (home_team === "Slovakia") {
+            setHomeFlag("SK");
+         }
+         else if (home_team === "Spain") {
+            setHomeFlag("ES");
+         }
+         else if (home_team === "Sweden") {
+            setHomeFlag("SE");
+         }
+         else if (home_team === "Ukraine") {
+            setHomeFlag("UA");
+         }
+         else if (home_team === "N.Ireland") {
+            setHomeFlag("JE");
+         }
+         else if (home_team === "Ireland") {
+            setHomeFlag("IE");
+         }
+         else if (home_team === "Russia") {
+            setHomeFlag("RU");
+         }
+         else if (home_team === "Belgium") {
+            setHomeFlag("BE");
+         }
+         else if (home_team === "Croatia") {
+            setHomeFlag("HR");
+         }
+         else if (home_team === "Czech Republic") {
+            setHomeFlag("CZ");
+         }
+         else if (home_team === "Italy") {
+            setHomeFlag("IT");
+         }
+         else if (home_team === "Hungary") {
+            setHomeFlag("HU");
+         }
+         else if (home_team === "Iceland") {
+            setHomeFlag("IS");
+         }
+         else if (home_team === "Portugal") {
+            setHomeFlag("PT");
+         }
+         else if (home_team === "Turkey") {
+            setHomeFlag("TR");
+         }
+         else if (home_team === "Austria") {
+            setHomeFlag("AT");
+         }
 
 
+
+         if (away_team === "Poland"){
+            setAwayFlag("PL")
+        } 
+        else if (away_team === "Albania") {
+           setAwayFlag("AL");
+        }
+        else if (away_team === "England") {
+            setAwayFlag("GB");
+         }
+         else if (away_team === "Germany") {
+            setAwayFlag("DE");
+         }
+         else if (away_team === "France") {
+            setAwayFlag("FR");
+         }
+         else if (away_team === "Romania") {
+            setAwayFlag("RO");
+         }
+         else if (away_team === "Switzerland") {
+            setAwayFlag("CH");
+         }
+         else if (away_team === "Wales") {
+            setAwayFlag("WS");
+         }
+         else if (away_team === "Slovakia") {
+            setAwayFlag("SK");
+         }
+         else if (away_team === "Spain") {
+            setAwayFlag("ES");
+         }
+         else if (away_team === "Sweden") {
+            setAwayFlag("SE");
+         }
+         else if (away_team === "Ukraine") {
+            setAwayFlag("UA");
+         }
+         else if (away_team === "N.Ireland") {
+            setAwayFlag("JE");
+         }
+         else if (away_team === "Ireland") {
+            setAwayFlag("IE");
+         }
+         else if (away_team === "Russia") {
+            setAwayFlag("RU");
+         }
+         else if (away_team === "Belgium") {
+            setAwayFlag("BE");
+         }
+         else if (away_team === "Croatia") {
+            setAwayFlag("HR");
+         }
+         else if (away_team === "Czech Republic") {
+            setAwayFlag("CZ");
+         }
+         else if (away_team === "Italy") {
+            setAwayFlag("IT");
+         }
+         else if (away_team === "Hungary") {
+            setAwayFlag("HU");
+         }
+         else if (away_team === "Iceland") {
+            setAwayFlag("IS");
+         }
+         else if (away_team === "Portugal") {
+            setAwayFlag("PT");
+         }
+         else if (away_team === "Turkey") {
+            setAwayFlag("TR");
+         }
+         else if (away_team === "Austria") {
+            setAwayFlag("AT");
+         }
+        
+    }, [])
     
 
     const [expanded, setExpanded] = React.useState(false);
@@ -66,16 +221,9 @@ function Game({event_id,eid_xml,eventname,grp,odds_1,odds_x,odds_2,status}) {
         
     }
 
-    useEffect(()=> {
-        const fetchGameId = async()=>{
-            const response = await instance.get(`Euro_events?eid_xml=${event_id}`)
-            setGameId(response.data[0].id)
-        }
-        
-        fetchGameId()
-    }, [])
     
-  
+    
+    
     const [formValues, setFormValues] = useState(initialValues)
     const customStyles = {
         content : {
@@ -161,7 +309,11 @@ function Game({event_id,eid_xml,eventname,grp,odds_1,odds_x,odds_2,status}) {
     //     }
     // }
 
+    const HomeFlag = Flags[homeFlag]
+    const AwayFlag = Flags[awayFlag]
+
     
+
     return (
         <>
         <div className="game_info">
@@ -175,10 +327,12 @@ function Game({event_id,eid_xml,eventname,grp,odds_1,odds_x,odds_2,status}) {
             {status}<br/>
             <div>{eventname}<br/></div>
             <button className="bet_btn" onClick={openLayBet}> Lay bet </button><br/>
-            
             <Card sx={{ maxWidth: 345 }}>
+    <HomeFlag width="30px" title="United States" className="..."/> 
       {eventname} 
-      <Flags.US title="United States" className="..."/>
+    <AwayFlag width="30px" title="United States" className="..."/>
+
+      
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           loremlorem
