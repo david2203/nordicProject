84557a132db4e9b995c733a6225084a1088462a1
Shawn Örnerstedt <Shawn.oernerstedt@emea.merkleinc.com>
2021-10-27 11:17:44 +0200

    no message

--- nordic_bet/src/components/Game/brackets.js
+++ nordic_bet/src/components/Game/brackets.js
@@ -1,4 +1,7 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
+import "../Brackets.css"
+import server from "../Global/config";
+import axios from "axios";
 
 function Brackets() {
 
@@ -10,6 +13,59 @@ function Brackets() {
     //     fetchGame()
     // }, [loadPage])
 
+	const chunked = [];
+	const useGetGames = () => {
+	  const [gamesArray, setGamesArray] = useState([]);
+	  const [loading, setLoading] = useState(true);
+  
+	  const groups = [
+		"EURO Grp. A",
+		"EURO Grp. B",
+		"EURO Grp. C",
+		"EURO Grp. D",
+		"EURO Grp. E",
+		"EURO Grp. F",
+	  ];
+  
+	  const instance = axios.create({ baseURL: server });
+  
+	  const fetchGames = async () => {
+		try {
+		  for (let i = 0; i < groups.length; i++) {
+			const { data } = await instance.get(`Euro_events?grp=${groups[i]}`);
+			setGamesArray((games) => [...games, ...data]);
+		  }
+		} catch (err) {
+		  console.log(err);
+		}
+  
+		setLoading(false);
+	  };
+  
+	  useEffect(() => {
+		fetchGames();
+	  }, []);
+  
+	  return { loading, gamesArray };
+	};
+  
+	const { loading, gamesArray } = useGetGames();
+  
+	if (!loading) {
+	  // All data should be available
+	  console.log(gamesArray);
+	  for (let i = 0; i < gamesArray.length; i += 6) {
+		chunked.push(gamesArray.slice(i, i + 6));
+	  }
+	}
+	console.log(chunked)
+	const counter = 0
+	for(let i = 0; i < chunked[0]; i++) {
+		if(chunked[0][i].status === "Finished"){
+			counter = counter ++
+		}
+	}
+	console.log(counter)
 
 
     return (
@@ -25,11 +81,11 @@ function Brackets() {
 	<link href='https://fonts.googleapis.com/css?family=Kaushan+Script|Herr+Von+Muellerhoff' rel='stylesheet' type='text/css'/>
 	<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'/>
 	<link href='https://fonts.googleapis.com/css?family=Istok+Web|Roboto+Condensed:700' rel='stylesheet' type='text/css'/>
-    <link rel="stylesheet" type="text/css" href="./Brackets.css"/>
+    <link rel="stylesheet" type="text/css" href="../Brackets.css"/>
 
 	<title>Uefa Euro 2016</title>
 <head/>
-<body>
+<body className="bracketsBody">
 	<header className="hero">
     <div className="hero-wrap">
      <p className="intro" id="intro">Nordic Bet</p>
@@ -46,36 +102,36 @@ function Brackets() {
 		<div className="round round-one current">
 			<div className="round-details">Åttondelsfinal<br/><span className="date">"DATE"</span></div>
 			<ul className="matchup">
-				<li className="team team-top">Switzerland<span className="score">1 (4)</span></li>
-				<li className="team team-bottom">Poland<span className="score">1 (5)</span></li>
+				<li className="team team-top">1A<span className="score">1 (4)</span></li>
+				<li className="team team-bottom">3CDE<span className="score">1 (5)</span></li>
 			</ul>
 			<ul className="matchup">
-				<li className="team team-top">Croatia<span className="score">0</span></li>
-				<li className="team team-bottom">Portugal<span className="score">1</span></li>
+				<li className="team team-top">1B<span className="score">0</span></li>
+				<li className="team team-bottom">3ACD<span className="score">1</span></li>
 			</ul>
 			<ul className="matchup">
-				<li className="team team-top">Wales<span className="score">1</span></li>
-				<li className="team team-bottom">Northern Ireland<span className="score">0</span></li>
+				<li className="team team-top">1C<span className="score">1</span></li>
+				<li className="team team-bottom">3ABF<span className="score">0</span></li>
 			</ul>
 			<ul className="matchup">
-				<li className="team team-top">Hungary<span className="score">0</span></li>
-				<li className="team team-bottom">Belgium<span className="score">4</span></li>
+				<li className="team team-top">1D<span className="score">0</span></li>
+				<li className="team team-bottom">3BEF<span className="score">4</span></li>
 			</ul>			
 			<ul className="matchup">
-				<li className="team team-top">Germany<span className="score">3</span></li>
-				<li className="team team-bottom">Slovakia<span className="score">0</span></li>
+				<li className="team team-top">1E<span className="score">3</span></li>
+				<li className="team team-bottom">2D<span className="score">0</span></li>
 			</ul>	
 			<ul className="matchup">
-				<li className="team team-top">Italy<span className="score">2</span></li>
-				<li className="team team-bottom">Spain<span className="score">0</span></li>
+				<li className="team team-top">1F<span className="score">2</span></li>
+				<li className="team team-bottom">2E<span className="score">0</span></li>
 			</ul>	
 			<ul className="matchup">
-				<li className="team team-top">France<span className="score">2</span></li>
-				<li className="team team-bottom">Republic of Ireland<span className="score">1</span></li>
+				<li className="team team-top">2A<span className="score">2</span></li>
+				<li className="team team-bottom">2C<span className="score">1</span></li>
 			</ul>
 			<ul className="matchup">
-				<li className="team team-top">England<span className="score">1</span></li>
-				<li className="team team-bottom">Iceland<span className="score">2</span></li>
+				<li className="team team-top">2B<span className="score">1</span></li>
+				<li className="team team-bottom">2F<span className="score">2</span></li>
 			</ul>										
 		</div>	
         {/* <!-- END ROUND ONE --> */}
