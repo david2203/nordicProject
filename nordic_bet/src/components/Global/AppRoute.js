78071aa00fa239bcbba8402bf23796537655aaa7
David Saupe <david.saupe@emea.merkleinc.com>
2021-10-29 11:04:09 +0200

    no message

--- nordic_bet/src/components/Global/AppRoute.js
+++ nordic_bet/src/components/Global/AppRoute.js
@@ -2,11 +2,10 @@ import React from "react";
 import Games from "../Game/Games"
 import Header from "./Header"
 import SignUp from "../Users/SignUp";
-import Profile from "../Users/Profile";
 import MyBets from "../Game/MyBets";
 import SignIn from "../Users/SignIn";
 import Admin from "../Admin/Admin";
-import Footer from "./Menu";
+import Footer from "./Footer";
 import Euro from "../Game/Euro"
 import ProfileInfo from "../Users/ProfileInfo";
 
@@ -34,7 +33,7 @@ function AppRoute() {
           <Route path="/SignUp" exact component={SignUp} />
           <Route path="/SignIn" exact component={SignIn} />
 
-          
+        <Footer/>
         </Router>
 
     
--- nordic_bet/src/components/Global/Header.js
+++ nordic_bet/src/components/Global/Header.js
@@ -20,6 +20,7 @@ function Header() {
             }
             fetchRole()
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
     const token = localStorage.getItem("jwt")
     function signOut() {
@@ -30,7 +31,7 @@ function Header() {
     return (
         <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light mx-auto">
-  <a className="navbar-brand ms-5 " href="#">Nordic Bet</a>
+<Link to="/Games" className="navbar-brand ms-5"> Nordic Bet</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
