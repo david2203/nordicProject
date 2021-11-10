22047bf56eb0ee812824e3333edb52ca39422798
David Saupe <david.saupe@emea.merkleinc.com>
2021-11-10 14:40:28 +0100

    no message

--- nordic_bet/src/components/Global/Header.js
+++ nordic_bet/src/components/Global/Header.js
@@ -5,30 +5,19 @@ import axios from "axios";
 import server from "./config";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap'
-
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 function Header() {
   const history = useHistory();
   const [isAdmin, setIsAdmin] = useState(false);
-  const [activeBets, setActiveBets] = useState(false)
-  const [activeBrackets, setActiveBrackets] = useState(false)
-  const [activeGames, setActiveGames] = useState(false)
-  const [activeAdmin, setActiveAdmin] = useState(false)
-  const [activeLogin, setActiveLogin] = useState(false)
-  const [activeScore, setActiveScore] = useState(false)
-  const [activeUpdate, setActiveUpdate] = useState(false)
-  const [activeReset, setActiveReset] = useState(false)
-  const [activeProfile, setActiveProfile] = useState(false)
-
-
-
-
-
-
-
+  const [activeBets, setActiveBets] = useState(false);
+  const [activeBrackets, setActiveBrackets] = useState(false);
+  const [activeGames, setActiveGames] = useState(false);
+  const [activeAdmin, setActiveAdmin] = useState(false);
+  const [activeScore, setActiveScore] = useState(false);
+  const [activeUpdate, setActiveUpdate] = useState(false);
+  const [activeReset, setActiveReset] = useState(false);
+  const [activeProfile, setActiveProfile] = useState(false);
 
   const instance = axios.create({ baseURL: server });
   useEffect(() => {
@@ -48,141 +37,274 @@ function Header() {
     history.push("/signin");
     window.location.reload();
   }
-function setAllFalse() {
-  setActiveBets(false)
-  setActiveBrackets(false)
-  setActiveGames(false)
-  setActiveAdmin(false)
-  setActiveLogin(false)
-  setActiveScore(false)
-  setActiveUpdate(false)
-  setActiveReset(false)
-  setActiveProfile(false)
-}
+  function setAllFalse() {
+    setActiveBets(false);
+    setActiveBrackets(false);
+    setActiveGames(false);
+    setActiveAdmin(false);
+    setActiveScore(false);
+    setActiveUpdate(false);
+    setActiveReset(false);
+    setActiveProfile(false);
+  }
   function activeMyBets() {
-    setAllFalse()
-    setActiveBets(true)
-    
+    setAllFalse();
+    setActiveBets(true);
   }
   function activeMyBrackets() {
-    setAllFalse()
-    setActiveBrackets(true)
-   
+    setAllFalse();
+    setActiveBrackets(true);
   }
   function activeMyGames() {
-    setAllFalse()
-    setActiveGames(true)
+    setAllFalse();
+    setActiveGames(true);
   }
   function activeMyAdmin() {
-    setAllFalse()
-    setActiveAdmin(true)
-    
+    setAllFalse();
+    setActiveAdmin(true);
   }
   function activeMyScore() {
-    setAllFalse()
-    setActiveScore(true)
-    
+    setAllFalse();
+    setActiveScore(true);
   }
   function activeMyUpdate() {
-    setAllFalse()
-    setActiveUpdate(true)
-    
+    setAllFalse();
+    setActiveUpdate(true);
   }
-   
+
   function activeMyReset() {
-    setAllFalse()
-    setActiveReset(true)
-    
+    setAllFalse();
+    setActiveReset(true);
   }
   function activeMyProfile() {
-    setAllFalse()
-    setActiveProfile(true)
-    
+    setAllFalse();
+    setActiveProfile(true);
   }
   return (
     <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        className="py-4 "
+      >
+        <Container fluid>
+          <Navbar.Brand href="/Games" style={{marginLeft:"5%"}} >Nordic Bet </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            {token ? (
+              <>
+                <Nav className="me-auto">
+                  {activeBets ? (
+                    <Link
+                      to="/MyBets"
+                      className="active "
+                      onClick={activeMyBets}
+                      style={{marginLeft:"5%"}}
+                      
+                    >
+                      Mina bets
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/MyBets"
+                      className="text-decoration-none "
+                      onClick={activeMyBets}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Mina bets
+                    </Link>
+                  )}
+                  {activeBrackets ? (
+                    <Link
+                      to="/Euro"
+                      className="active"
+                      onClick={activeMyBrackets}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Brackets
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/Euro"
+                      className="text-decoration-none pl-3 ml-3"
+                      onClick={activeMyBrackets}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Brackets
+                    </Link>
+                  )}
+                  {activeGames ? (
+                    <Link
+                      to="/Games"
+                      className="active"
+                      onClick={activeMyGames}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Spel
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/Games"
+                      className="text-decoration-none pl-3 ml-3"
+                      onClick={activeMyGames}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Spel
+                    </Link>
+                  )}
+                  {activeScore ? (
+                    <Link
+                      to="/Scoreboard"
+                      className="active"
+                      onClick={activeMyScore}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Top-lista
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/Scoreboard"
+                      className="text-decoration-none pl-3 ml-3"
+                      onClick={activeMyScore}
+                      style={{marginLeft:"5%"}}
+                    >
+                      Top-lista
+                    </Link>
+                  )}
 
-
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" className="py-4 ">
-  <Container fluid>
-  <Navbar.Brand href="/Games">Nordic Bet </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    
-    {token ? (
-        <>  
-        <Nav className="me-auto "> 
-        {activeBets? <Link to="/MyBets" className="active" onClick={activeMyBets}>Mina bets</Link> :
-         <Link to="/MyBets" className="text-decoration-none " onClick={activeMyBets}>Mina bets</Link>
-         }   
-        {activeBrackets? <Link to="/Euro" className="active" onClick={activeMyBrackets} >Brackets</Link> :
-        <Link to="/Euro" className="text-decoration-none pl-3 ml-3" onClick={activeMyBrackets}>Brackets</Link>
-        }
-        {activeGames? <Link to="/Games" className="active" onClick={activeMyGames} >Spel</Link> :
-        <Link to="/Games" className="text-decoration-none pl-3 ml-3" onClick={activeMyGames}>Spel</Link>
-        }
-        {activeScore? <Link to="/Scoreboard" className="active" onClick={activeMyScore} >Top-lista</Link> :
-        <Link to="/Scoreboard" className="text-decoration-none pl-3 ml-3" onClick={activeMyScore}>Top-lista</Link>
-        }
-      
-      {/* {isAdmin ? ( 
+                  {/* {isAdmin ? ( 
       <Link to="/Admin">Admin</Link>
       ) : (
        <></>
       )} */}
-      
-      {isAdmin ? ( 
-      <NavDropdown title="Adminpanel" id="collasible-nav-dropdown">
-        {activeAdmin? <Link to="/Admin" className="active" onClick={activeMyAdmin} >Rätta spel</Link> :
-        <Link to="/Admin" className="text-decoration-none pl-3 ml-3" onClick={activeMyAdmin}>Rätta spel</Link>
-        }
-       <br/>
-       {activeUpdate? <Link to="/Update" className="active" onClick={activeMyUpdate} >Updatera elimination</Link> :
-        <Link to="/Update" className="text-decoration-none pl-3 ml-3" onClick={activeMyUpdate}>Updatera elimination</Link>
-        }
 
-        <NavDropdown.Divider />
+                  {isAdmin ? (
+                    <NavDropdown
+                      title="Adminpanel"
+                      id="collasible-nav-dropdown"
+                    >
+                      {activeAdmin ? (
+                        <Link
+                          to="/Admin"
+                          className="active"
+                          onClick={activeMyAdmin}
+                          style={{marginLeft:"5%"}}
+                        >
+                          Rätta spel
+                        </Link>
+                      ) : (
+                        <Link
+                          to="/Admin"
+                          className="text-decoration-none pl-3 ml-3"
+                          onClick={activeMyAdmin}
+                          style={{marginLeft:"5%"}}
+                        >
+                          Rätta spel
+                        </Link>
+                      )}
+                      <br />
+                      {activeUpdate ? (
+                        <Link
+                          to="/Update"
+                          className="active"
+                          onClick={activeMyUpdate}
+                          style={{marginLeft:"5%"}}
+                        >
+                          Updatera elimination
+                        </Link>
+                      ) : (
+                        <Link
+                          to="/Update"
+                          className="text-decoration-none pl-3 ml-3"
+                          onClick={activeMyUpdate}
+                          style={{marginLeft:"5%"}}
+                        >
+                          Updatera elimination
+                        </Link>
+                      )}
 
-        {activeReset? <Link to="/Reset"  className="active" onClick={activeMyReset}  style={{color: 'red'}}> <i className="bi bi-exclamation-triangle "> </i> Nollställ event </Link> :
-        <Link to="/Reset"  className="text-decoration-none" onClick={activeMyReset}  style={{color: 'red'}}> <i className="bi bi-exclamation-triangle "> </i>Nollställ event </Link>
-        }
-       
-      </NavDropdown>
-       ) : (
-        <></>
-       )}
-      </Nav>
-      <Nav >    
-        {activeProfile ? <Link to="/Profiel" className="active" onClick={activeMyProfile}> Profil </Link> :
-        <Link to="/Profiel" className="text-decoration-none pl-3 ml-3" onClick={activeMyProfile}> Profil </Link> }
-      
-      <Link eventKey={2} className="text-decoration-none pl-3 ml-3" onClick={signOut}>
-        Logga ut
-      </Link>
-    </Nav>
-      </>
-    
+                      <NavDropdown.Divider />
+
+                      {activeReset ? (
+                        <Link
+                          to="/Reset"
+                          className="active"
+                          onClick={activeMyReset}
+                          style={{ color: "red", marginLeft:"5%"}}
+                        >
+                          {" "}
+                          <i className="bi bi-exclamation-triangle "> </i>{" "}
+                          Nollställ event{" "}
+                        </Link>
+                      ) : (
+                        <Link
+                          to="/Reset"
+                          className="text-decoration-none"
+                          onClick={activeMyReset}
+                          style={{ color: "red", marginLeft:"5%" }}
+                        >
+                          {" "}
+                          <i className="bi bi-exclamation-triangle "> </i>
+                          Nollställ event{" "}
+                        </Link>
+                      )}
+                    </NavDropdown>
+                  ) : (
+                    <></>
+                  )}
+                </Nav>
+                <Nav>
+                  {activeProfile ? (
+                    <Link
+                      to="/Profiel"
+                      className="active"
+                      onClick={activeMyProfile}
+                      style={{marginLeft:"5%"}}
+                    >
+                      {" "}
+                      Profil{" "}
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/Profiel"
+                      className="text-decoration-none pl-3 ml-3"
+                      onClick={activeMyProfile}
+                      style={{marginLeft:"5%"}}
+                    >
+                      {" "}
+                      Profil{" "}
+                    </Link>
+                  )}
+
+                  <Link
+                    eventKey={2}
+                    className="text-decoration-none pl-3 ml-3"
+                    onClick={signOut}
+                  >
+                    Logga ut
+                  </Link>
+                </Nav>
+              </>
             ) : (
               <>
-              <Nav className="me-auto" >
+                <Nav className="me-auto"></Nav>
+                <Nav>
+                  <Link to="/SignIn">Login</Link>
+                  <Link eventKey={2} to="/SignUp">
+                    Registrera
+                  </Link>
                 </Nav>
-        <Nav >    
-
-      <Link to="/SignIn">Login</Link>
-      <Link eventKey={2} to="/SignUp">
-        Registrera
-      </Link>
-    </Nav>
-  
-</>)}
-
-);
-</Navbar.Collapse>
-  </Container>
-</Navbar>
-</>
-     
-   
-  )}
+              </>
+            )}
+            );
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
 
 export default Header;
